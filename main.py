167f7ece94cfd27d44d12025f0dc0de5398be8f7
Fábio Costa <datasorcery@deltasigma.com.br>
2020-07-21 14:05:37 -0300

    Recebendo <ID> na URL

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import os
 
 from flask import Flask
+from flask import request
+
+import json
 
 app = Flask(__name__)
 
@@ -9,8 +12,12 @@ def hello_world():
     target = os.environ.get('TARGET', 'World')
     return 'Hello {}!\n'.format(target)
 
-@app.route('/sensor')
-def sensor(request = None):
+# GET /sensor/<id> -> Retorna última leitura do sensor
+# PUT /sensor/<id> -> Salva leitura do sensor
+
+@app.route('/sensor', methods=['GET', 'PUT'])
+@app.route('/sensor/<id>', methods=['GET', 'PUT'])
+def sensor(id=None):
     """HTTP Cloud Funtion.
     Args:
         request (flask.Request): The request object.
@@ -23,7 +30,19 @@ def sensor(request = None):
     #request_json = request.get_json(silent=True)
     #request_args = request.args
 
-    return ('OK', 200)
+    if id is not None:
+        print('id')
+        ret = {'id': id, 'temp': 25, 'humid': 75}
+    else:
+        ret = 'NO <ID>'
+
+    return (ret, 200)
+
+def sensor_get():
+    pass
+
+def sensor_put():
+    pass
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
