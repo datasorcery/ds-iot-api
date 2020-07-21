8224d0ef2d38dacea182a643e4c43dbcd36503e1
Fábio Costa <datasorcery@deltasigma.com.br>
2020-07-21 18:43:04 -0300

    Funcao para salvar e recuperar dados implementada

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import os
+from os import path
+import json
+from datetime import datetime
+import pytz
 
 from flask import Flask
 from flask import request
 
-import json
+from google.cloud import firestore
 
 app = Flask(__name__)
+
+# Setup credential for local development
+pwd = path.dirname(path.abspath(__file__))
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="{}/gcp.json".format(pwd)
+
 
 @app.route('/')
 def hello_world():
@@ -36,7 +45,7 @@ def sensor(dev_id):
         if request.method == 'GET':
             ret = sensor_get(dev_id)
         elif request.method == 'PUT':
-            ret = sensor_put()
+            ret = sensor_put(dev_id)
         else:
             ret = ('Method Not Allowed', 405)
     
@@ -45,11 +54,41 @@ def sensor(dev_id):
     return ret
 
 def sensor_get(dev_id):
-    return ({'id': dev_id, 'temp': 25, 'humid': 75}, 200)
+    # Create Firestore Client
+    db = firestore.Client()
 
-def sensor_put():
-    print('sensor_put')
-    return ('PUT', 200)
+    # Get data from this sensor
+    sensor_ref = db.collection(u'nodemcu_TH_{}'.format(dev_id))
+    sensor_docs = sensor_ref.stream()
+
+    readings = list()
+
+    for read in sensor_docs:
+        readings.append(read.to_dict())
+
+    return (json.dumps(readings), 200)
+    #return ({'id': dev_id, 'temp': 25, 'humid': 75}, 200)
+
+def sensor_put(dev_id):
+    # Plumbing
+    request_json = request.get_json(silent=True)
+    
+    # Create Firestore Client
+    db = firestore.Client()
+
+    # Document name -> time in YYYYMMDDHHMMSS GMT
+    doc_name = datetime.now().astimezone(pytz.utc).strftime('%Y%m%d%H%M%S')
+
+    # Add new reading 
+    doc_ref = db.collection(u'nodemcu_TH_{}'.format(dev_id)).document(u'{}'.format(doc_name))
+    doc_ref.set({
+        u'temp': request_json['temp'],
+        u'humid': request_json['humid'],
+        u'timestamp': doc_name
+    })
+  
+    print('sensor_put:', request_json['temp'], request_json['humid'])
+    return ('OK', 200)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
